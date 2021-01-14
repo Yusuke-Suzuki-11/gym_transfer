f58ac655a600e3fccc825c05838329c12b48e15b
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-01-14 21:53:32 +0900

    reset.css追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,15 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-
 		<title>With gymnastics | 動的タイトル</title>
 		<link href="/css/app.css" rel="stylesheet" type="text/css">
 	</head>
-	<body class="antialiased">
-		<p class="test">test</p>
+	<header>
+		@yield('header')
+	</header>
 
+	<body class="antialiased">
+		<p class="test">テスト</p>
+		@yield('content')
 	</body>
 </html>
