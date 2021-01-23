b3d2b5c1dff3f4d1cc4ea79f98d49562b4a9a087
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-01-23 10:24:46 +0900

    エラ〜メッセージを実装

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,19 +6,19 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<title>With gymnastics | 動的タイトル</title>
 		<link href="/css/app.css" rel="stylesheet" type="text/css">
-
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet">
 
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	</head>
 	<header>
 		@include('elements.header')
 	</header>
 
 	<body>
-		<div class="content">
 			@yield('content')
-		</div>
+			
+
 	</body>
 </html>
--- resources/views/multi_auth/login.blade.php
+++ resources/views/multi_auth/login.blade.php
@@ -2,29 +2,39 @@
 
 @section('content')
 	@if(Session::has("flash_message"))
-		<p class="alert">{{ Session::get("flash_message") }}</p>
+		<div id="session-error">
+			<p class="session-error-message" >ログアウトしました</p>
+		</div>
 	@endif
-	<div class="auth-logo">
-		<img src="image/logo.png" alt="ロゴ">
-		<span>With Gymnastics</span>
-	</div>
-	<div class="auth-container">
-		<div class="auth-top">
-			<p class="auth-top-title">ログイン</p>
-			<span>※ウィズ体操クラブ会員専用サイトです</span>
+	<div class="content">
+		<div class="auth-logo">
+			<img src="image/logo.png" alt="ロゴ">
+			<span>With Gymnastics</span>
 		</div>
+		<div class="auth-container">
+			<div class="auth-top">
+				<p class="auth-top-title">ログイン</p>
+				<span>※ウィズ体操クラブ会員専用サイトです</span>
+			</div>
 
-		<div class="auth-form">
-			<form action="{{route('login')}}" method="POST">
-				@csrf
-				<input class="auth-login-input" type="text" placeholder="メールアドレス" name="email">
+			<div class="auth-form">
+				<form action="{{route('login')}}" method="POST">
+					@csrf
+					<input class="auth-login-input" type="text" placeholder="メールアドレス" name="email">
 
-				<input class="auth-login-input" type="password" placeholder="パスワード" name="password">
+					<input class="auth-login-input" type="password" placeholder="パスワード" name="password">
 
-				<input type="hidden" name="guard_type" value="students">
+					<input type="hidden" name="guard_type" value="students">
 
-				<button class="auth-submit-btn" type="submit">ログイン</button>
-			</form>
+					<button class="auth-submit-btn" type="submit">ログイン</button>
+				</form>
+			</div>
 		</div>
 	</div>
+	<script>
+		$('#session-error').hide().fadeIn(1000);
+		setTimeout(() => {
+			$('#session-error').fadeOut(1000);
+		}, 3000);
+	</script>
 @endsection
