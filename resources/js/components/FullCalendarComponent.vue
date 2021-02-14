caf7bdd1d0c07d1f8acdafc057df1828797b475e
Yusukei-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-02-14 21:59:10 +0900

    カレンダー日修正、下地作成

--- resources/js/components/FullCalendarComponent.vue
+++ resources/js/components/FullCalendarComponent.vue
@@ -2,6 +2,8 @@
 import FullCalendar from "@fullcalendar/vue";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { formatDate } from "@fullcalendar/vue";
 
 export default {
   components: {
@@ -15,16 +17,21 @@ export default {
   data() {
     return {
       calendarOptions: {
-        plugins: [dayGridPlugin, interactionPlugin],
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        },
         initialView: "dayGridMonth",
-        dateClick: this.handleDateClick,
         events: this.dateData,
+        eventClick: this.test,
       },
     };
   },
   methods: {
-    handleDateClick: function (arg) {
-      alert("date click! " + arg.dateStr);
+    test: function () {
+      alert("test");
     },
   },
 };
