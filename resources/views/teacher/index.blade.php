5d86a30d65165b6eb5b79ce097cb31e7941eca47
Yusukei-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-02-17 20:16:34 +0900

    css修正

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layout')
 
 @section('content')
+@include('elements.tc_sidebar')
 <div class="tc-index">
-	@include('elements.tc_sidebar')
-
 	<div class="tc-main">
 		本日の練習一覧
 	</div>
+
 </div>
 
 @endsection
