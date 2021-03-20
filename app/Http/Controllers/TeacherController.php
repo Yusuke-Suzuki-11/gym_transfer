cf3359fea652dcf38afe40ecaec4c1e25921264b
Yusukei-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-03-20 15:30:04 +0900

    練習の登録処理完了

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Lesson;
+use App\Models\LessonDate;
 use App\Models\LessonTime;
 use Illuminate\Http\Request;
 use App\Models\Student;
@@ -37,22 +38,41 @@ class TeacherController extends Controller
 			$targetDate = date('Y-m-d', strtotime($targetDate . "+1 day"));
 		}
 
-		return view('teacher.calendar.edit')->with(['start' => $start, 'dateArray' => $dateArray]);
+		return view('teacher.calendar.edit')->with([
+			'start' => $start,
+			'dateArray' => $dateArray,
+			'LessonDateInstance' => new LessonDate
+		]);
 	}
 
 	public function calendar_update(Request $request)
 	{
-
 		$dateArray = json_decode($request->dateArray);
 
+
 		foreach ($dateArray as $date) {
-			// 日付に入力がなければスキップ
-			if (!isset($request[$date])) {
-				continue;
+			$LessonDateRow = LessonDate::all()->where('date', $date)->isEmpty() ? new LessonDate() : LessonDate::where('date', $date)->first();
+			$dateItem = $request[$date];
+
+			if (LessonDate::all()->where('date', $date)->isEmpty()) {
+				$LessonDateRow = new LessonDate();
+				$LessonDateRow->date = $date;
+				if (!isset($dateItem)) {
+					$LessonDateRow->save();
+					continue;
+				}
+			} else {
+				$LessonDateRow = LessonDate::where('date', $date)->first();
 			}
+
+			$LessonDateRow->floor_flag = isset($dateItem['floor']) ? $dateItem['floor'] : null;
+			$LessonDateRow->bar_flag = isset($dateItem['bar']) ? $dateItem['bar'] : null;
+			$LessonDateRow->vaulting_flag = isset($dateItem['vaulting']) ? $dateItem['vaulting'] : null;
+			$LessonDateRow->trampoline_flag = isset($dateItem['trampoline']) ? $dateItem['trampoline'] : null;
+			$LessonDateRow->other_flag = isset($dateItem['other']) ? $dateItem['other'] : null;
+			$LessonDateRow->save();
 		}
 
-
-		dd($request->all());
+		return redirect(route('tc.lesson.calendar'));
 	}
 }
--- resources/views/teacher/calendar/edit.blade.php
+++ resources/views/teacher/calendar/edit.blade.php
@@ -32,6 +32,9 @@
 					{{intval(date('m', strtotime($start))) . '月'}}
 				</div>
 				@foreach ($dateArray as $date)
+					@php
+						$LessonDateRow = !$LessonDateInstance->where('date', $date)->get()->isEmpty() ? $LessonDateInstance->where('date', $date)->first() : '';
+					@endphp
 					<div class="tc-date-item-box {{intval(date('w', strtotime($date))) == 0 || intval(date('w', strtotime($date))) == 6 ? 'yattaze' : '' }}">
 						<div class="tc-date-title">
 							<p class="tc-date-txt">
@@ -42,23 +45,23 @@
 						<div class="tc-date-form">
 							@csrf
 							<div class="tc-date-form-check">
-								<input type="checkbox" value="1" name="{{$date}}[floor]" id="{{$date}}-floor">
+								<input type="checkbox" value="1" name="{{$date}}[floor]" id="{{$date}}-floor" {{ isset($LessonDateRow->floor_flag) ? 'checked' : '' }}>
 								<label for="{{$date}}-floor">マット</label>
 							</div>
 							<div class="tc-date-form-check">
-								<input type="checkbox" value="1" name="{{$date}}[bar]" id="{{$date}}-bar">
+								<input type="checkbox" value="1" name="{{$date}}[bar]" id="{{$date}}-bar" {{ isset($LessonDateRow->bar_flag) ? 'checked' : '' }}>
 								<label for="{{$date}}-bar">鉄棒</label>
 							</div>
 							<div class="tc-date-form-check">
-								<input type="checkbox" value="1" name="{{$date}}[vaulting]" id="{{$date}}-vaulting">
+								<input type="checkbox" value="1" name="{{$date}}[vaulting]" id="{{$date}}-vaulting" {{isset($LessonDateRow->vaulting_flag) ? 'checked' : '' }}>
 								<label for="{{$date}}-vaulting">とび箱</label>
 							</div>
 							<div class="tc-date-form-check">
-								<input type="checkbox" value="1" name="{{$date}}[trampoline]" id="{{$date}}-trampoline">
+								<input type="checkbox" value="1" name="{{$date}}[trampoline]" id="{{$date}}-trampoline" {{isset($LessonDateRow->trampoline_flag) ? 'checked' : '' }}>
 								<label for="{{$date}}-trampoline">トランポリン</label>
 							</div>
 							<div class="tc-date-form-check">
-								<input type="checkbox" value="1" name="{{$date}}[other]" id="{{$date}}-other">
+								<input type="checkbox" value="1" name="{{$date}}[other]" id="{{$date}}-other" {{isset($LessonDateRow->other_flag) ? 'checked' : '' }}>
 								<label for="{{$date}}-other">その他</label>
 							</div>
 						</div>
