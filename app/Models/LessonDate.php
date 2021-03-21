4f8ce3bbae67ac61c571097a8c268cbb5c0f24e2
Yusukei-Suzuki-11 <mr.suzuki.11@gmail.com>
2021-03-21 17:18:44 +0900

    スペース修正

--- app/Models/LessonDate.php
+++ app/Models/LessonDate.php
@@ -9,35 +9,35 @@ use function PHPUnit\Framework\isNull;
 
 class LessonDate extends Model
 {
-    use HasFactory;
+	use HasFactory;
 
-    protected $fillable = [
-        'date',
-        'floor_flag',
-        'bar_flag',
-        'vaulting_flag',
-        'trampoline_flag',
-        'other_flag',
-    ];
+	protected $fillable = [
+		'date',
+		'floor_flag',
+		'bar_flag',
+		'vaulting_flag',
+		'trampoline_flag',
+		'other_flag',
+	];
 
-    public function hasFloorFlag($date)
-    {
-        return !is_null($this->where('date', $date)->first()->floor_flag);
-    }
+	public function hasFloorFlag($date)
+	{
+		return !is_null($this->where('date', $date)->first()->floor_flag);
+	}
 
-    public function getTodayLessonType($date)
-    {
-        $LessonDateRow = LessonDate::where('date', $date)->first();
-        $todayLessons = [];
+	public function getTodayLessonType($date)
+	{
+		$LessonDateRow = LessonDate::where('date', $date)->first();
+		$todayLessons = [];
 
-        $todayLessons[] = isset($LessonDateRow->floor_flag) ? config('const.LESSON_TYPE')['floor'] : '';
-        $todayLessons[] = isset($LessonDateRow->bar_flag) ? config('const.LESSON_TYPE')['bar'] : '';
-        $todayLessons[] = isset($LessonDateRow->vaulting_flag) ? config('const.LESSON_TYPE')['vaulting'] : '';
-        $todayLessons[] = isset($LessonDateRow->trampoline_flag) ? config('const.LESSON_TYPE')['trampoline'] : '';
-        $todayLessons[] = isset($LessonDateRow->other_flag) ? config('const.LESSON_TYPE')['other'] : '';
+		$todayLessons[] = isset($LessonDateRow->floor_flag) ? config('const.LESSON_TYPE')['floor'] : '';
+		$todayLessons[] = isset($LessonDateRow->bar_flag) ? config('const.LESSON_TYPE')['bar'] : '';
+		$todayLessons[] = isset($LessonDateRow->vaulting_flag) ? config('const.LESSON_TYPE')['vaulting'] : '';
+		$todayLessons[] = isset($LessonDateRow->trampoline_flag) ? config('const.LESSON_TYPE')['trampoline'] : '';
+		$todayLessons[] = isset($LessonDateRow->other_flag) ? config('const.LESSON_TYPE')['other'] : '';
 
-        $todayLessons = array_filter($todayLessons, 'strlen');
+		$todayLessons = array_filter($todayLessons, 'strlen');
 
-        return implode('/', $todayLessons);
-    }
+		return implode('/', $todayLessons);
+	}
 }
